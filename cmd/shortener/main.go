f8fce0a28d356e3f7e8d6ccdac0a49c6429f82d8
foilhatguy <antony.aleynikov@gmail.com>
2023-01-28 17:44:22 +0400

    Increment 1 done

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,51 +2,22 @@ package main
 
 import (
 	"fmt"
-	"github.com/FoilHatGuy/url_shortener_go_practice/storage"
-	"io"
+	"github.com/FoilHatGuy/url_shortener_go_practice/handlers"
 	"log"
 	"net/http"
 )
 
 const ( //config
-	urlLength = 10
-	host      = "localhost"
-	port      = 8080
+	host = "localhost"
+	port = 8080
 )
-
-var database = new(storage.Storage)
 
 func main() {
 	server := http.Server{
 		Addr: fmt.Sprintf("%s:%d", host, port),
 	}
 
-	http.HandleFunc("/", receiveUrl)
-	//http.HandleFunc("/:id", provideUrl)
-	//http.HandleFunc("/*", provideUrl)
+	http.HandleFunc("/", handlers.ReceiveUrl)
 
 	log.Fatal(server.ListenAndServe())
-}
-
-func receiveUrl(writer http.ResponseWriter, request *http.Request) {
-	switch request.Method {
-	case "POST":
-		urlBytes, _ := io.ReadAll(request.Body)
-		url := string(urlBytes)
-
-		shortUrl := database.AddUrl(url, urlLength)
-
-		fmt.Printf("Input url: %s\n", url)
-		fmt.Printf("SHort url: %s\n\n", shortUrl)
-
-		writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		_, err := writer.Write([]byte(shortUrl))
-		if err != nil {
-			return
-		}
-
-	case "GET":
-		fmt.Printf("Input url: %s\n", request.URL.Fragment)
-
-	}
 }
--- cmd/shortener/storage/temporary.go
+++ cmd/shortener/storage/temporary.go
@@ -1,17 +1,43 @@
 package storage
 
-import "github.com/FoilHatGuy/url_shortener_go_practice/urlGenerator"
+import (
+	"errors"
+	"fmt"
+	"github.com/FoilHatGuy/url_shortener_go_practice/urlGenerator"
+)
 
-type longUrls []string
-type shortUrls []string
-type Storage struct {
-	longUrls
-	shortUrls
+// type longUrls []string
+// type shortUrls []string
+type dataT map[string]string
+
+type storage struct {
+	//longUrls
+	//shortUrls
+	data dataT
 }
 
-func (s *Storage) AddUrl(url string, urlLength int) string {
-	s.longUrls = append(s.longUrls, url)
+var Database = new(storage)
+
+//Database.data =
+
+func (s *storage) AddUrl(url string, urlLength int) string {
+	if Database.data == nil {
+		Database.data = make(dataT)
+	}
 	short := urlGenerator.RandSeq(urlLength)
-	s.shortUrls = append(s.shortUrls, short)
+	s.data[short] = url
+	//s.shortUrls = append(s.shortUrls, short)
 	return short
+}
+
+func (s *storage) GetUrl(url string) (string, error) {
+	if Database.data == nil {
+		Database.data = make(dataT)
+	}
+	val, ok := s.data[url]
+	fmt.Print(ok, "\n")
+	if ok {
+		return val, nil
+	}
+	return "", errors.New("no url")
 }
