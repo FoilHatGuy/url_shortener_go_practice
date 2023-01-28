94cc69b057d14bb6358a9673e6e24d3bd823051c
foilhatguy <antony.aleynikov@gmail.com>
2023-01-28 19:25:30 +0400

    Fix status code

--- cmd/shortener/handlers/handlers.go
+++ cmd/shortener/handlers/handlers.go
@@ -28,6 +28,7 @@ func ReceiveURL(writer http.ResponseWriter, request *http.Request) {
 
 		writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		result := host + ":" + strconv.FormatInt(port, 10) + "/" + shortURL
+		writer.WriteHeader(201)
 		_, err := writer.Write([]byte(result))
 		if err != nil {
 			return
