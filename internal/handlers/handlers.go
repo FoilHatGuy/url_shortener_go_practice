64ea46e1f684322d938c91459593912c41ebff9c
foilhatguy <antony.aleynikov@gmail.com>
2023-01-28 19:12:16 +0400

    refactor for go vet

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -15,19 +15,19 @@ const ( //config
 	port      = 8080
 )
 
-func ReceiveUrl(writer http.ResponseWriter, request *http.Request) {
+func ReceiveURL(writer http.ResponseWriter, request *http.Request) {
 	switch request.Method {
 	case "POST":
 		urlBytes, _ := io.ReadAll(request.Body)
 		url := string(urlBytes)
 
-		shortUrl := storage.Database.AddUrl(url, urlLength)
+		shortURL := storage.Database.AddURL(url, urlLength)
 
 		fmt.Printf("Input url: %s\n", url)
-		fmt.Printf("Short url: %s\n\n", shortUrl)
+		fmt.Printf("Short url: %s\n\n", shortURL)
 
 		writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		result := host + ":" + strconv.FormatInt(port, 10) + "/" + shortUrl
+		result := host + ":" + strconv.FormatInt(port, 10) + "/" + shortURL
 		_, err := writer.Write([]byte(result))
 		if err != nil {
 			return
@@ -37,7 +37,7 @@ func ReceiveUrl(writer http.ResponseWriter, request *http.Request) {
 		urlArray := strings.Split(request.URL.Path[1:], "/")
 		fmt.Printf("Input url: %s\n", urlArray[0])
 		if len(urlArray) == 1 && len(urlArray[0]) == urlLength {
-			url, err := storage.Database.GetUrl(urlArray[0])
+			url, err := storage.Database.GetURL(urlArray[0])
 			fmt.Printf("Output url: %s, %t\n", url, err == nil)
 			if err == nil {
 				fmt.Printf("get complete\n\n")
