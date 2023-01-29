f8be95a817c40c26a463c30383428d7e8063c37f
foilhatguy <antony.aleynikov@gmail.com>
2023-01-29 12:38:14 +0400

    redone using chi

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/go-chi/chi/v5"
 	"log"
 	"net/http"
 	"shortener/internal/handlers"
@@ -13,11 +14,15 @@ const ( //config
 )
 
 func main() {
+	r := chi.NewRouter()
 	server := http.Server{
 		Addr: fmt.Sprintf("%s:%d", host, port),
 	}
 
-	http.HandleFunc("/", handlers.ReceiveURL)
+	r.Post("/", handlers.SendURL)
+	r.Get("/{shortURL:[a-zA-Z]{10}}", handlers.ReceiveURL)
+
+	//http.HandleFunc("/", handlers.ReceiveURL)
 
 	log.Fatal(server.ListenAndServe())
 }
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -2,11 +2,11 @@ package handlers
 
 import (
 	"fmt"
+	"github.com/go-chi/chi/v5"
 	"io"
 	"net/http"
 	"shortener/internal/storage"
 	"strconv"
-	"strings"
 )
 
 const ( //config
@@ -15,39 +15,39 @@ const ( //config
 	port      = 8080
 )
 
+func SendURL(writer http.ResponseWriter, request *http.Request) {
+	urlBytes, _ := io.ReadAll(request.Body)
+	url := string(urlBytes)
+
+	shortURL := storage.Database.AddURL(url, urlLength)
+
+	fmt.Printf("Input url: %s\n", url)
+	fmt.Printf("Short url: %s\n\n", shortURL)
+
+	writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	result := "http://" + host + ":" + strconv.FormatInt(port, 10) + "/" + shortURL
+	writer.WriteHeader(201)
+	_, err := writer.Write([]byte(result))
+	if err != nil {
+		return
+	}
+
+}
+
 func ReceiveURL(writer http.ResponseWriter, request *http.Request) {
-	switch request.Method {
-	case "POST":
-		urlBytes, _ := io.ReadAll(request.Body)
-		url := string(urlBytes)
-
-		shortURL := storage.Database.AddURL(url, urlLength)
-
-		fmt.Printf("Input url: %s\n", url)
-		fmt.Printf("Short url: %s\n\n", shortURL)
-
-		writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		result := "http://" + host + ":" + strconv.FormatInt(port, 10) + "/" + shortURL
-		writer.WriteHeader(201)
-		_, err := writer.Write([]byte(result))
-		if err != nil {
-			return
-		}
-
-	case "GET":
-		urlArray := strings.Split(request.URL.Path[1:], "/")
-		fmt.Printf("Input url: %s\n", urlArray[0])
-		if len(urlArray) == 1 && len(urlArray[0]) == urlLength {
-			url, err := storage.Database.GetURL(urlArray[0])
-			fmt.Printf("Output url: %s, %t\n", url, err == nil)
-			if err == nil {
-				fmt.Printf("get complete\n\n")
-				writer.Header().Set("Location", url)
-				writer.WriteHeader(307)
-				_, _ = writer.Write([]byte(url))
-			} else {
-				writer.WriteHeader(http.StatusBadRequest)
-			}
+	url := chi.URLParam(request, "shortURL")
+	fmt.Printf("Input url: %s\n", url)
+	if len(url) == urlLength {
+		url, err := storage.Database.GetURL(url)
+		fmt.Printf("Output url: %s, %t\n", url, err == nil)
+		if err == nil {
+			fmt.Printf("get complete\n\n")
+			writer.Header().Set("Location", url)
+			writer.WriteHeader(307)
+			_, _ = writer.Write([]byte(url))
+		} else {
+			writer.WriteHeader(http.StatusBadRequest)
 		}
 	}
+
 }
