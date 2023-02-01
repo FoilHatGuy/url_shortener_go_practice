f53f70399bc5a4bdbb7cc6d8e6541a2b47cd740e
Anton Aleynikov <antony.aleynikov@gmail.com>
2023-02-01 15:19:24 +0400

    main branch rollback all changes

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,23 +1,3 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-	"shortener/internal/handlers"
-)
-
-const ( //config
-	host = "localhost"
-	port = 8080
-)
-
-func main() {
-	server := http.Server{
-		Addr: fmt.Sprintf("%s:%d", host, port),
-	}
-
-	http.HandleFunc("/", handlers.ReceiveURL)
-
-	log.Fatal(server.ListenAndServe())
-}
+func main() {}
