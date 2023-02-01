9f538d1761aeec6a13fdf41d5ffbb26b65cfcbdf
Anton Aleynikov <antony.aleynikov@gmail.com>
2023-02-01 15:34:26 +0400

    patch from all other branches because git was broken

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,28 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"github.com/go-chi/chi/v5"
+	"log"
+	"net/http"
+	"shortener/internal/handlers"
+)
+
+const ( //config
+	host = "localhost"
+	port = 8080
+)
+
+func main() {
+	r := chi.NewRouter()
+	server := http.Server{
+		Addr: fmt.Sprintf("%s:%d", host, port),
+	}
+
+	r.Post("/", handlers.SendURL)
+	r.Get("/{shortURL:[a-zA-Z]{10}}", handlers.ReceiveURL)
+
+	//http.HandleFunc("/", handlers.ReceiveURL)
+
+	log.Fatal(server.ListenAndServe())
+}
---
+++ internal/handlers/handlers.go
@@ -0,0 +1,53 @@
+package handlers
+
+import (
+	"fmt"
+	"github.com/go-chi/chi/v5"
+	"io"
+	"net/http"
+	"shortener/internal/storage"
+	"strconv"
+)
+
+const ( //config
+	urlLength = 10
+	host      = "localhost"
+	port      = 8080
+)
+
+func SendURL(writer http.ResponseWriter, request *http.Request) {
+	urlBytes, _ := io.ReadAll(request.Body)
+	url := string(urlBytes)
+
+	shortURL := storage.Database.AddURL(url, urlLength)
+
+	fmt.Printf("Input url: %s\n", url)
+	fmt.Printf("Short url: %s\n\n", shortURL)
+
+	writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	result := "http://" + host + ":" + strconv.FormatInt(port, 10) + "/" + shortURL
+	writer.WriteHeader(201)
+	_, err := writer.Write([]byte(result))
+	if err != nil {
+		return
+	}
+
+}
+
+func ReceiveURL(writer http.ResponseWriter, request *http.Request) {
+	url := chi.URLParam(request, "shortURL")
+	fmt.Printf("Input url: %s\n", url)
+	if len(url) == urlLength {
+		url, err := storage.Database.GetURL(url)
+		fmt.Printf("Output url: %s, %t\n", url, err == nil)
+		if err == nil {
+			fmt.Printf("get complete\n\n")
+			writer.Header().Set("Location", url)
+			writer.WriteHeader(307)
+			_, _ = writer.Write([]byte(url))
+		} else {
+			writer.WriteHeader(http.StatusBadRequest)
+		}
+	}
+
+}
