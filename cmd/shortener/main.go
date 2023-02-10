0b0387392166ff520ab05c7c6f56835551bc3dba
Anton Aleynikov <antony.aleynikov@gmail.com>
2023-02-10 11:41:34 +0400

    added flags with priority over .env

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	_ "github.com/sakirsensoy/genv/dotenv/autoload"
+	"shortener/internal/cfg"
 	"shortener/internal/server"
 )
 
 func main() {
+	cfg.Init()
 	server.Run()
-
 }
--- internal/cfg/cfg.go
+++ internal/cfg/cfg.go
@@ -1,20 +1,42 @@
 package cfg
 
-import "github.com/sakirsensoy/genv"
+import (
+	"flag"
+	"github.com/sakirsensoy/genv"
+)
+
+var (
+	server_adress     string
+	base_url          string
+	file_storage_path string
+)
+
+func Init() {
+	flag.StringVar(&server_adress, "a",
+		genv.Key("SERVER_HOST").Default("localhost").String(),
+		"help message for flagname")
+	flag.StringVar(&base_url, "b",
+		genv.Key("BASE_URL").Default("/").String(),
+		"help message for flagname")
+	flag.StringVar(&file_storage_path, "f",
+		genv.Key("FILE_STORAGE_PATH").Default("./data").String(),
+		"help message for flagname")
+	flag.Parse()
+}
 
 var Shortener = &shortCfg{
 	UrlLength: genv.Key("SHORT_URL_LENGTH").Default(10).Int(),
 }
 
 var Server = &serverCfg{
-	Host: genv.Key("SERVER_HOST").Default("localhost").String(),
+	Host: server_adress,
 	Port: genv.Key("SERVER_PORT").Default("8080").String(),
 }
 var Router = &routerCfg{
-	BaseURL: genv.Key("BASE_URL").Default("/").String(),
+	BaseURL: base_url,
 }
 var Storage = &storageCfg{
 	AutosaveInterval: genv.Key("STORAGE_AUTOSAVE_INTERVAL").Default(60).Int(),
-	SavePath:         genv.Key("FILE_STORAGE_PATH").Default("./data").String(),
+	SavePath:         file_storage_path,
 	StorageType:      genv.Key("STORAGE_TYPE").Default("file").String(),
 }
