dd94870c1d946289b380ff2b80d62d75cbd58484
Anton Aleynikov <antony.aleynikov@gmail.com>
2023-06-20 17:32:23 +0400

    forgot to add log.Fatal

--- internal/server/serverrun.go
+++ internal/server/serverrun.go
@@ -62,7 +62,7 @@ func Run(config *cfg.ConfigT) {
 
 		certPEM, certKey, err := authEngine.GetCertificate()
 		if err != nil {
-			log.Fatalf("certificate generation failed: %w: %s\n", err)
+			log.Fatalf("certificate generation failed: %s\n", err)
 		}
 		if err := srv.ListenAndServeTLS(certPEM, certKey); err != nil && err != http.ErrServerClosed {
 			log.Fatalf("listen: %s\n", err)
